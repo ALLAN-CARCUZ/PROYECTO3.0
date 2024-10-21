a0a2a8c3b5337156a1d498b3b445f00482776eaa
Johann Abraham Argueta Mendoza <johannargueta38@gmail.com>
2024-10-21 14:37:53 -0600

    AUTH en reservaciones

--- public/scripts/scriptreservacionesauth.js
+++ public/scripts/scriptreservacionesauth.js
@@ -1,18 +1,46 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const authButton = document.getElementById('authButton');
-    const registerButton = document.getElementById('registerButton');
+    const authButton = document.querySelector('.auth-button');
+    const registerButton = document.querySelector('.register-button');
     const userGreeting = document.getElementById('userGreeting');
-    const token = localStorage.getItem('token');
+    const rol = localStorage.getItem('rol');
     const username = localStorage.getItem('username');
 
-    // Verifica si el token existe, si existe, actualiza el saludo y oculta los botones
-    if (token) {
+    // Si hay un nombre de usuario almacenado, ocultar botones de autenticación y mostrar saludo
+    if (username) {
         authButton.textContent = 'Cerrar Sesión';
-        userGreeting.textContent = `Hola, ${username}`;
+        authButton.style.display = 'block';
         registerButton.style.display = 'none';
+        userGreeting.textContent = `Hola, ${username}`;
     } else {
         authButton.textContent = 'Iniciar Sesión';
+        registerButton.style.display = 'block';
         userGreeting.textContent = '';
-        registerButton.style.display = 'inline';
     }
+
+    authButton.addEventListener('click', () => {
+        if (username) {
+            localStorage.removeItem('token');
+            localStorage.removeItem('username');
+            localStorage.removeItem('rol');
+            window.location.href = 'login.html';
+        } else {
+            window.location.href = 'login.html';
+        }
+    });
+
+    // Función para ocultar botones de admin si el rol no es "admin"
+    function manageAdminButtons() {
+        const botonesAdmin = document.querySelectorAll('.admin-only');
+        if (rol === 'admin') {
+            botonesAdmin.forEach(boton => {
+                boton.style.display = 'inline-block';
+            });
+        } else {
+            botonesAdmin.forEach(boton => {
+                boton.style.display = 'none';
+            });
+        }
+    }
+
+    manageAdminButtons();
 });
