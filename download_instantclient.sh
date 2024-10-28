3cc1bc0a3211fa878a9171b160de4a488990904f
Lrodriguezm11 <126182568+Lrodriguezm11@users.noreply.github.com>
2024-10-28 01:50:44 -0600

    libreria oracle 5.0

---
+++ download_instantclient.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Crear el directorio para Oracle Instant Client si no existe
+mkdir -p instantclient/instantclient_21_16
+
+# Función para descargar desde Google Drive
+download_from_drive() {
+    FILE_ID=$1
+    FILE_NAME=$2
+    curl -L -o "instantclient/instantclient_21_16/$FILE_NAME" "https://drive.google.com/uc?export=download&id=$FILE_ID"
+}
+
+# Descarga de los archivos necesarios
+download_from_drive "1u_vgyWY3mO_FfouL1LOICzdFNhWObE9K" "libclntsh.so.21.1"
+download_from_drive "1YNB1immIYWep2QORD7gyNdFsOS0qXT2G" "libociei.so"
+download_from_drive "13ME6YksV0fDPZ8Zj2dHFEjRTPQ9TDp5D" "libnnz21.so"
